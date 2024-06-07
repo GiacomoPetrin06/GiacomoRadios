cfe68eed322c9541cdc2bb9231e2e727fad4dc9d
GiacomoPetrin <GiacomoPetrin@DESKTOP-521CBM9>
2024-06-07 11:08:49 +0200

    Add project files.

---
+++ Code/Radio.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GiacomoRadios.Code
+{
+    public class Radio
+    {
+        // Radio Propriety
+        public String Id { get; set; }
+        public String Title { get; set; }
+        public String UrlAudio { get; set; }
+        public String Genere { get; set; }
+        public String ImgIcona { get; set; }
+
+        // Radio Constructor
+        public Radio(String id, String title, String urlAudio, String genere, String ImgIcona)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.UrlAudio = urlAudio;
+            this.Genere = genere;
+            this.ImgIcona = ImgIcona;
+        }
+        public Radio(String id, String title)
+        {
+            this.Id = id;
+            this.Title = title;
+        }
+
+        // Radio Default final Constructor
+        public Radio()
+        {
+            this.Id = "0";
+            this.Title = "x";
+            this.UrlAudio = "x";
+            this.Genere = "x";
+            this.ImgIcona = "x";
+        }
+    }
+}
---
+++ Code/RadioService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
+
+namespace GiacomoRadios.Code
+{
+    public class RadioService
+    {
+        private HttpClient _http;
+        public RadioService(HttpClient http)
+        {
+            _http = http;
+            List<Radio> list = new List<Radio>();
+        }
+
+        public async Task<List<Radio>> GetRadios()
+        {
+            List<Radio> radios = await _http.GetFromJsonAsync<List<Radio>>("radioList/radios.json");
+            return radios ?? [];
+            // se radios è null, mi restituisce un array vuoto
+        }
+    }
+}
